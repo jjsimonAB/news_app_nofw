2ff36ae740d7fa6d8a0b325df8393d52bf344159
Jeffry J. Simon <simon.dev93@gmail.com>
2020-11-11 09:58:10 -0400

    router implemented

--- app/index.php
+++ app/index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo phpinfo();
+require __DIR__ . '/vendor/autoload.php';
+
+use Src\router\Request;
+use Src\router\Response;
+use Src\router\Router;
+
+Router::get('/', function () {
+    echo 'Hello World';
+});
+
+Router::get('/post/([0-9]*)', function (Request $req, Response $res) {
+    $res->toJSON([
+        'post' => ['id' => $req->params[0]],
+        'status' => 'ok',
+    ]);
+});
